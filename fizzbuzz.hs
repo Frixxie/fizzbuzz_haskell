b7178f2878c6d71633768f568f128b8b45634e93
Fredrik <freddahf@outlook.com>
2021-06-30 12:18:43 +0200

    some improvements

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,13 +1,11 @@
-fizzbuzz :: Int -> String
-fizzbuzz val
-    | val `mod` (a * b) == 0 = a_out ++ b_out
-    | val `mod` a == 0       = a_out
-    | val `mod` b == 0       = b_out
-    | otherwise              = show val
-    where a = 3
-          b = 5
-          a_out = "fizz"
-          b_out = "buzz"
+fizzbuzz :: (Int, Int, Int) -> String
+fizzbuzz (val, a, b) | val `mod` (a * b) == 0 = a_out ++ b_out
+                     | val `mod` a == 0       = a_out
+                     | val `mod` b == 0       = b_out
+                     | otherwise              = show val
+ where
+  a_out = "fizz"
+  b_out = "buzz"
 
 main = do
-    mapM (putStrLn . fizzbuzz) [1..2^8]
+  mapM (putStrLn . fizzbuzz) [ (x, 3, 5) | x <- [1 .. 2 ^ 20] ]
